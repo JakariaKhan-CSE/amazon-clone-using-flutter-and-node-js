06d6146560d3c86dd5d70c04e9a125ede2a9f2ad
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-04-26 11:48:45 +0600

    use ongenerateRoute for avoid much time write MaterialPageRoute

---
+++ lib/router.dart
@@ -0,0 +1,22 @@
+import 'package:amazon_clone/features/auth/screen/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings)
+{
+  switch(routeSettings.name)
+      {
+    case AuthScreen.routeName:
+      {
+        return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => AuthScreen(),);
+      }
+
+    default:
+      {
+        return MaterialPageRoute(builder: (_) => Scaffold(
+          body: Center(child: Text('Screen does not exist'),),
+        ),);
+      }
+  }
+}
