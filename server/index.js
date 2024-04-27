dbd99fd86b93e8c0960eded6d32ff7d1612dd734
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-04-27 21:17:16 +0600

    use json formatter (first number) in chrome extention to see data json format

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const PORT = 3000;
 
 const app = express();
 
+
+// Creating an API
+// http:<your ip address>/hello-world
+app.get('/hello-world',(req,res)=>{
+        // res.send("Hello World"); // aita simple dekhabe
+
+        res.json({hello: "Hello World"});       // aita json format a dekhabe
+});
+
+
 //"0.0.0.0" aita use kora hoye karon android emulator localhost find out korte pare na.
 app.listen(PORT, "0.0.0.0", (req,res)=>{   // also use function() {} and ()=>{}.    This are the same
     console.log('Connected at port: '+PORT);
