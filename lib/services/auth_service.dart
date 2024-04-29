af439b2eaff2ac453c0927c15461f9c206191e2e
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-04-29 09:49:29 +0600

    use authservice class this look differnt constructor see this.  void signUpUser({     required String email,     required String name,     required String password,   }) async {     try{
        }catch(e)
        {
          print(e);
        }
      }

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+class AuthService {
+  // signUp user
+  void signUpUser({
+    required String email,
+    required String name,
+    required String password,
+  }) async {
+    try{
+
+    }catch(e)
+    {
+      print(e);
+    }
+  }
+}
