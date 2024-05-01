5fbdebd2dbb73371b3abc9773ed9174fd646fd4e
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-05-01 20:39:39 +0600

    i catch error on backend auth.js not middleware auth.js and auth-service get user

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,30 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// const auth = async (req,res,next)=>{
-//     try {
-//         console.log('auth middleware call');
-//         const token = req.header('x-auth-token');
-//         console.log(`token is in middleware: ${token}`);
-//         if(!token) // 401 for unauthorized access
-//         return res.status(401).json({msg: "No auth token, access denied"});
-// console.log('Find token');
-//         const verified = jwt.verify(token, "passwordkey");
-//         if(!verified) return res.status(401).json({msg: "Token verification failed, authorization denied"});
-// console.log('Token verified');
-//         req.user = verified.id;
-//         res.token = token;
-//         console.log(`res.user: ${req.user},   res.token: ${res.token}`);
-//         next();
-//     } catch (error) {
-//         res.status(500).json({error: error.message});
-//     }
-// }
-
-// module.exports = auth; // i get long time error because i use module.exports(auth)
-
-
-
-
 
 const jwt = require("jsonwebtoken");
 
