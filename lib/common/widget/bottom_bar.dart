becbcb62b98a585ff43651fec060a9a30f24cace
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-05-02 09:25:25 +0600

    bottom nav bar a page add kora  hoyese

--- lib/common/widget/bottom_bar.dart
+++ lib/common/widget/bottom_bar.dart
@@ -1,6 +1,7 @@
 import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:amazon_clone/features/home/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:badges/badges.dart' as badges;  // very important
 class BottomBar extends StatefulWidget {
   static const String routeName = '/actual-home';  // const na dile error ashbe
   const BottomBar({super.key});
@@ -13,15 +14,28 @@ class _BottomBarState extends State<BottomBar> {
   int _page = 0;
   double bottomBarwidth = 42;
   double bottomBarBorderwidth = 5;
+  void updatePage(int page)
+  {
+    setState(() {
+      _page = page;
+    });
+  }
+  List<Widget> pages = [
+    const HomeScreen(),
+    const Center(child: Text('Account page'),),
+    const Center(child: Text('Cart page'),),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[_page],
     bottomNavigationBar: BottomNavigationBar(
       currentIndex: _page,
       selectedItemColor: GlobalVariables.selectedNavBarColor,
       unselectedItemColor: GlobalVariables.unselectedNavBarColor,
       backgroundColor: GlobalVariables.backgroundColor,
       iconSize: 28,
+      onTap: updatePage,  // (val){updatePage(val)}. this is write short type
       items: [
 BottomNavigationBarItem(icon: Container(
   width: bottomBarwidth,
@@ -53,9 +67,16 @@ BottomNavigationBarItem(icon: Container(
                   ,width: bottomBarBorderwidth
               ))
           ),
-          child: Icon(Icons.account_circle),
+            
+              child: badges.Badge(
+                  badgeContent: Text('3'),
+badgeStyle: badges.BadgeStyle(
+  elevation: 0,
+  badgeColor: Colors.white,
+),
+                  child: Icon(Icons.add_shopping_cart_outlined))),
 
-        ),label: ''),
+        label: ''),
       ],
     ),
     );
