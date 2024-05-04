b3a510f7a9e0fd9929cfb537be3e8f9bf80bbf7f
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-05-04 10:12:12 +0600

    admin appbar design

--- lib/features/admin/screen/admin_screen.dart
+++ lib/features/admin/screen/admin_screen.dart
@@ -1,4 +1,8 @@
+import 'package:amazon_clone/provider/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../constants/global_variable.dart';
 
 class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key});
@@ -10,8 +14,39 @@ class AdminScreen extends StatefulWidget {
 class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
+      appBar: PreferredSize(
+        // default appbar na niye  aita neyar karon hosse appbar er height issa moto korbo
+        preferredSize: Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(gradient: GlobalVariables.appBarGradient),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                // alignment: Alignment.topRight,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                child: Text(user.type,style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,fontWeight: FontWeight.bold
+                ),),
+              )
+            ],
+          ),
 
+        ),
+
+      ),
     );
   }
 }
