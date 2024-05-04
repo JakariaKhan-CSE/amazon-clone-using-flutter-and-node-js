12b4fdc603b81a71d620f5e94265114532832f26
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-05-04 21:15:30 +0600

    if write filepicker code. needed to stop and install app in emulator again. otherwise get error. also care full to use dart:io not dart:html. after new install it get permission from user

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,6 +1,30 @@
+import 'dart:io'; // aita default dart:html thakbe aita change na korle error ashbe(dart:io)
+// this is very important
+import 'package:flutter/widgets.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-void showSnackbar(BuildContext context,String text, Color color)
-{
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text,style: TextStyle(color: color),)));
+void showSnackbar(BuildContext context, String text, Color color) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+    text,
+    style: TextStyle(color: color),
+  )));
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  try {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(type: FileType.image, allowMultiple: true);
+
+    if (result != null) {
+      List<File> images = result.paths.map((path) => File(path!)).toList();
+    } else {
+      // User canceled the picker
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return images;
 }
--- lib/features/admin/screen/add_product_screen.dart
+++ lib/features/admin/screen/add_product_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io'; // not import dart:html, only import this from another action
+
 import 'package:amazon_clone/common/widget/customButton.dart';
 import 'package:amazon_clone/common/widget/custom_textfield.dart';
+import 'package:amazon_clone/constants/utils.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +20,7 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
+  List<File> images = [];
   final TextEditingController productNameController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
@@ -39,6 +44,13 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion'
   ];
+
+  void selectImage()async{
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
@@ -75,38 +87,41 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 SizedBox(
                   height: 20,
                 ),
-                DottedBorder(
-                    strokeCap: StrokeCap.round,
-                    borderType: BorderType
-                        .RRect, // default rectangle thake aitake rounded korarar jonno aita
-                    radius: Radius.circular(10),
-                    dashPattern: [
-                      10,
-                      4
-                    ], // prothom ta each dotted er size barabe, r 2 number ta dotted number barabe
-                    child: Container(
-                      width: double.infinity,
-                      height: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.folder_open,
-                            size: 40,
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            'Select Product Images',
-                            style: TextStyle(
-                                fontSize: 15, color: Colors.grey.shade400),
-                          )
-                        ],
-                      ),
-                    )),
+                GestureDetector(
+                  onTap: selectImage,
+                  child: DottedBorder(
+                      strokeCap: StrokeCap.round,
+                      borderType: BorderType
+                          .RRect, // default rectangle thake aitake rounded korarar jonno aita
+                      radius: Radius.circular(10),
+                      dashPattern: [
+                        10,
+                        4
+                      ], // prothom ta each dotted er size barabe, r 2 number ta dotted number barabe
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.folder_open,
+                              size: 40,
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Select Product Images',
+                              style: TextStyle(
+                                  fontSize: 15, color: Colors.grey.shade400),
+                            )
+                          ],
+                        ),
+                      )),
+                ),
                 SizedBox(
                   height: 30,
                 ),
