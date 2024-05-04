6fd762cde17070e140c3ee4745e2ad053c04ff4c
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-05-04 15:55:05 +0600

    admin add product UI design complete

--- lib/common/widget/custom_textfield.dart
+++ lib/common/widget/custom_textfield.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
+
 class CustomTextField extends StatelessWidget {
   final String hintText;
   final TextEditingController controller;
-  const CustomTextField({super.key, required this.controller, required this.hintText});
+  final int maxline;
+  const CustomTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      // required this.maxline    // aivabe dile entire code modify korte hobe tai
+        this.maxline = 1   // default value 1 set kore dissi jodi user na dei aita otherwise user jeita dibe seita
+      });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      maxLines: maxline,
       controller: controller,
       decoration: InputDecoration(
-        hintText: hintText,
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.black38)
-        ),
-        enabledBorder:  OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black38)
-        )
-      ),
-      validator: (val){
-        if(val == null || val.isEmpty)
-          {
-            return 'Enter your $hintText'; // see this
-          }
+          hintText: hintText,
+          border:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.black38)),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black38))),
+      validator: (val) {
+        if (val == null || val.isEmpty) {
+          return 'Enter your $hintText'; // see this
+        }
         return null;
       },
     );
--- lib/features/admin/screen/add_product_screen.dart
+++ lib/features/admin/screen/add_product_screen.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_clone/common/widget/customButton.dart';
+import 'package:amazon_clone/common/widget/custom_textfield.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,6 +16,29 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
+  final TextEditingController productNameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+
+  @override
+  void dispose() {
+    // dispose are very important for memory management. don't ignore it
+    priceController.dispose();
+    productNameController.dispose();
+    quantityController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
+  String category = 'Mobiles'; // for default show
+  List<String> productCategories = [
+    'Mobiles',
+    'Essentials',
+    'Appliances',
+    'Books',
+    'Fashion'
+  ];
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
@@ -24,32 +50,108 @@ class _AddProductScreenState extends State<AddProductScreen> {
           flexibleSpace: Container(
             decoration: BoxDecoration(gradient: GlobalVariables.appBarGradient),
           ),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                // alignment: Alignment.topRight,
-                child: Image.asset(
-                  'assets/images/amazon_in.png',
-                  width: 120,
-                  height: 45,
-                  color: Colors.black,
-                ),
-              ),
-              Container(
-                child: Text(user.type,style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,fontWeight: FontWeight.bold
-                ),),
-              )
-            ],
+          title: Text(
+            'Add Product',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
           ),
-          leading: IconButton(onPressed: (){
-            Navigator.pop(context);
-          },icon: Icon(Icons.arrow_back_ios,size: 22,),),
-
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 22,
+            ),
+          ),
         ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                DottedBorder(
+                    strokeCap: StrokeCap.round,
+                    borderType: BorderType
+                        .RRect, // default rectangle thake aitake rounded korarar jonno aita
+                    radius: Radius.circular(10),
+                    dashPattern: [
+                      10,
+                      4
+                    ], // prothom ta each dotted er size barabe, r 2 number ta dotted number barabe
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.folder_open,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Select Product Images',
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey.shade400),
+                          )
+                        ],
+                      ),
+                    )),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                    controller: productNameController,
+                    hintText: 'Product Name'),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  controller: descriptionController,
+                  hintText: 'Description',
+                  maxline: 7,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(controller: priceController, hintText: 'Price'),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                    controller: quantityController, hintText: 'Quantity'),
+                SizedBox(
+                    width: double.infinity,  // entire width a click korlei dropdown show korbe
+                    child: DropdownButton(
+                      value: category,
+                      icon: Icon(Icons.arrow_drop_down_outlined),
+                      items: productCategories.map((String item) {
+                        return DropdownMenuItem(value: item, child: Text(item));
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          category = newValue!;
+                        });
+                      },
+                    )),
+                SizedBox(height: 10,),
+                CustomButton(text: 'Sell', onTap: (){
 
+                })
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
